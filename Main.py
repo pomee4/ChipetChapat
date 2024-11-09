29a668a74c3540cdaa87eaabebab9b168a6a2700
pomee4 <pomee4@protonmail.com>
2024-11-09 18:57:58 +0100

    Forrás URL-ek beolvasása, CSV-k letöltése.

--- Main.py
+++ Main.py
@@ -1,1 +1,34 @@
 #Main.py - Ez a program fog elindulni előszor a program futásakor.
+
+# Importáljuk a szükséges modulokat
+import sqlite3
+import csv
+import requests
+import yaml
+import os
+
+# Forrás URL-ek beolvasása
+with open('forrasok.yaml', 'r') as file:
+    data = yaml.safe_load(file)
+
+# Save path definiálása
+save_path = 'raw_data'
+
+# URL-ek beolvasása és CSV-k letöltése
+for entry in data['forras']:
+    file_id = entry['id']
+    url = entry['url']
+    filename = os.path.join(save_path, f"{file_id}.csv")  # CSV fájlok mentése a raw_data almappába
+
+    try:
+        # GET request küldése a megadott URL-re
+        response = requests.get(url)
+
+        # CSV mentése
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+
+
+        print("{filename} letöltve")
+    except requests.exceptions.RequestException as e:
+        print("Hiba a letöltésben innen: {url}.")
