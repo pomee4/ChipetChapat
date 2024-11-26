90dc6021e917591d182bc6fa3a67cd9e62ec10d6
pomee4 <pomee4@protonmail.com>
2024-11-26 17:43:31 +0100

    HTML és styling kimozgatása

--- app/routes.py
+++ app/routes.py
@@ -20,108 +20,19 @@ def register(app):
 
     @app.route('/form1')
     def form1():
-        # Az első form tartalma
-        return '''
-            <style>
-                a:link, a:visited {
-                    background-color: black;
-                    color: white;
-                    padding: 10px 20px;
-                    font-size: 24px;
-                    text-align: center;
-                    text-decoration: none;
-                    display: inline-block;
-                    border-radius: 10px;
-                }
-                a:hover, a:active {
-                    background-color: grey;
-                }
-            </style>
-            <h1>Első Form</h1>
-            <img src="/static/images/intro_1.png" alt="Intro 1 kép"width="500" height="500">
-            <p>Ez az első oldal tartalma...</p>
-            <a href="/form2">Következő oldal</a>
-        '''
+        return render_template('form1.html')
 
     @app.route('/form2')
     def form2():
-        # A második form tartalma
-        return '''
-            <style>
-                a:link, a:visited {
-                    background-color: black;
-                    color: white;
-                    padding: 10px 20px;
-                    font-size: 24px;
-                    text-align: center;
-                    text-decoration: none;
-                    display: inline-block;
-                    border-radius: 10px;
-                }
-                a:hover, a:active {
-                    background-color: grey;
-                }
-            </style>
-            <h1>Második Form</h1>
-            <img src="/static/images/intro_2.png" alt="Intro 2 kép">
-            <p>Ez a második oldal tartalma...</p>
-            <a href="/form3">Következő oldal</a>
-        '''
+        return render_template('form2.html')
 
     @app.route('/form3')
     def form3():
-        # A harmadik form tartalma
-        return '''
-            <style>
-                a:link, a:visited {
-                    background-color: black;
-                    color: white;
-                    padding: 10px 20px;
-                    font-size: 24px;
-                    text-align: center;
-                    text-decoration: none;
-                    display: inline-block;
-                    border-radius: 10px;
-                }
-                a:hover, a:active {
-                    background-color: grey;
-                }
-            </style>
-            <h1>Harmadik Form</h1>
-            <div style="position: absolute; top: 24%; left: 23%; transform: translate(-50%, -50%); width: 150px; text-align: center; font-family: Arial, sans-serif; padding: 10px; border-radius: 10px;">
-                <p style="font-size: 18px; margin: 0; color:black;">Nézünk fogyasztási adatokat?</p>
-            </div>
-            <img src="/static/images/intro_3.png" alt="Intro 3 kép">
-            <p>Ez a harmadik oldal tartalma...</p>
-            <!--<a href="/jump_form">Következő oldal</a>-->
-            <a href="/form4">Következő oldal</a>
-        '''
+        return render_template('form3.html')
 
-    # Form route for charts
     @app.route('/form4')
     def form4():
-        return '''
-            <style>
-                a:link, a:visited {
-                    background-color: black;
-                    color: white;
-                    padding: 10px 20px;
-                    font-size: 24px;
-                    text-align: center;
-                    text-decoration: none;
-                    display: inline-block;
-                    border-radius: 10px;
-                }
-                a:hover, a:active {
-                    background-color: grey;
-                }
-            </style>
-            <h2>Az adatok a KSH adatbázisából valók!</h2>
-            <a href="/line_chart">Vonal diagram</a>
-            <a href="/bar_chart">Oszlop diagram</a>
-            <a href="/regression_chart">Lineáris regresszió</a>
-            <a href="/">Vissza a kezdőoldalra</a>
-        '''
+        return render_template('form4.html')
 
     @app.route('/line_chart')
     def line_chart():
